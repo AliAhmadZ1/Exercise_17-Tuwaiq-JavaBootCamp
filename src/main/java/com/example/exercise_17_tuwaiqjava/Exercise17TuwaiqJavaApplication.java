6ab2e4c58f821641ae0becfb4a9f98f064d48c78
aliali <aliali@test.com>
2025-04-08 14:04:28 +0300

    first commit

---
+++ src/main/java/com/example/exercise_17_tuwaiqjava/Exercise17TuwaiqJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise_17_tuwaiqjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise17TuwaiqJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise17TuwaiqJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exercise_17_tuwaiqjava/Exercise17TuwaiqJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise_17_tuwaiqjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise17TuwaiqJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
